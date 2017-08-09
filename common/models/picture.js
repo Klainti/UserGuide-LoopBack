5632926cc1388054c5380a92cf62c4cbe9ede713
Klainti <kbontourri@gmail.com>
2017-08-09 17:59:55 +0300

    beforeRemote download image from given url

--- common/models/picture.js
+++ common/models/picture.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(Picture) {
-  
+const request = require('request');
+
+module.exports = (Picture) => {
+  /* Download image from given url */
+  Picture.beforeRemote('create', (ctx, modelInstance, next) => {
+    request.get({ url: ctx.req.body.url, encoding: 'binary' }, (err, response, body) => {
+      if (err) {
+        next(err);
+      } else if (response.statusCode !== 200) {
+        next(response.statusCode);
+      } else {
+        const buffer = new Buffer(body, 'binary');
+        ctx.req.body.data = buffer;
+        next();
+      }
+    });
+  });
+
 };
