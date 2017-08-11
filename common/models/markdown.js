708cb2dc2f1f8944a4ddb58fd6a8e779a3126601
Klainti <kbontourri@gmail.com>
2017-08-11 14:48:46 +0300

    Save Markdown and folders!

--- common/models/markdown.js
+++ common/models/markdown.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const showdown = require('showdown');
+const config = require('../../server/config.json');
 
 const converter = new showdown.Converter();
+const PathValidation = new RegExp(config.PathValidation, 'm');
 
-/* Convert a markdown to html! */
 module.exports = (Markdown) => {
+  /* Convert a markdown text to html! */
   Markdown.preview = (data, cb) => {
     try {
       const html = converter.makeHtml(data);
@@ -19,5 +21,32 @@ module.exports = (Markdown) => {
     accepts: { arg: 'data', type: 'string' },
     returns: { arg: 'html', type: 'string' },
     http: { path: '/preview', verb: 'get' }
+  });
+
+  /* Create folders */
+  Markdown.beforeRemote('upsertWithWhere', (ctx, modelInstance, next) => {
+    if (!PathValidation.test(ctx.req.body.path)){
+      next('Invalid Path');
+    } else {
+      if (ctx.req.body.path !== '/') {
+        const SplittedPath = ctx.req.body.path.split('/');
+        let LenOfPath = SplittedPath.length;
+        const Folders = [];
+        while (LenOfPath !== 1) { /* Split path to take folders! */
+          const name = SplittedPath[LenOfPath - 1];
+          let path = SplittedPath.slice(0, LenOfPath - 1).join('/');
+          if (path === '') {
+            path = '/';
+          }
+          Folders.push({ name, path });
+          LenOfPath--;
+        }
+        Markdown.app.FS.SaveFolder(Folders)
+          .catch((error) => {
+            next(error);
+          });
+      }
+      next();
+    }
   });
 };
