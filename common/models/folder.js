b171c259edda39f80db5dac1425ee05488cf6f5a
Klainti <kbontourri@gmail.com>
2017-08-11 17:44:16 +0300

    Get children of a folder

--- common/models/folder.js
+++ common/models/folder.js
@@ -1,5 +1,21 @@
 'use strict';
 
-module.exports = function(Folder) {
-
+module.exports = (Folder) => {
+  Folder.afterRemote('findById', (ctx, modelInstance, next) => {
+    let ChildrenPath;
+    if (modelInstance.path === '/') {
+      ChildrenPath = `${modelInstance.path}${modelInstance.name}`;
+    } else {
+      ChildrenPath = `${modelInstance.path}/${modelInstance.name}`;
+    }
+    Folder.app.FS.getTreeByPath(ChildrenPath)
+      .then((children) => {
+        const list = Folder.app.catalog.CreateList(children);
+        ctx.result = { list, path: list[0].path };
+        next();
+      })
+      .catch((error) => {
+        next(error);
+      });
+  });
 };
