0650399fba1a637b87cbf9ffdf3fc9fb2911e87d
Klainti <kbontourri@gmail.com>
2017-08-14 17:42:27 +0300

    Remove showContent script. Add this logic as a remote method

--- common/models/folder.js
+++ common/models/folder.js
@@ -1,6 +1,25 @@
 'use strict';
 
 module.exports = (Folder) => {
+  /* Search a folder by path and get its content */
+  Folder.getContent = (path, cb) => {
+    Folder.app.FS.getTreeByPath(path)
+      .then((children) => {
+        cb(null, children);
+      })
+      .catch((error) => {
+        cb(error, null);
+      });
+  };
+
+  Folder.remoteMethod('getContent', {
+    accepts: { arg: 'path', type: 'string' },
+    returns: { arg: 'list', type: 'string' },
+    description: 'Get content of a folder',
+    http: { path: '/getContent', verb: 'get' }
+  });
+
+  /* Search a folder by id and get its content. */
   Folder.afterRemote('findById', (ctx, modelInstance, next) => {
     let ChildrenPath;
     if (modelInstance.path === '/') {
