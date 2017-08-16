e74727f346170f12b2229fd0c10a06d57681c6d7
Klainti <kbontourri@gmail.com>
2017-08-16 12:57:08 +0300

    Convert folder content to list

--- common/models/folder.js
+++ common/models/folder.js
@@ -5,7 +5,8 @@ module.exports = (Folder) => {
   Folder.getContent = (path, cb) => {
     Folder.app.FS.getTreeByPath(path)
       .then((children) => {
-        cb(null, children);
+        const list = Folder.app.utils.CreateList(children);
+        cb(null, list);
       })
       .catch((error) => {
         cb(error, null);
