ccc8916c11061165bd77a20c61041793d177f5b7
vchristodoulou <vchristodoulou1@gmail.com>
2017-08-17 12:06:44 +0300

    editor and userGuide buttons to change states

--- client/components/app.component.js
+++ client/components/app.component.js
@@ -1,6 +1,32 @@
 'use strict';
 
 class AppController {
+  constructor($state, Folder) {
+    this.$state = $state;
+    this.Folder = Folder;
+  }
+  userGuideBtn() {
+    this.path = '/'; // TODO binding not loading correctly
+    this.Folder.getContent({ path: '/' }, (res) => {
+      this.catalog = res.list;
+      this.$state.go('guide', {id: '59944d89925bec7138ef580f' });
+    }, (error) => {
+      console.log(error.message);
+      this.catalog = undefined;
+      this.path = '';
+    });
+  }
+  editorBtn() {
+    this.path = '/'; // TODO binding not loading correctly
+    this.Folder.getContent({ path: '/' }, (res) => {
+      this.catalog = res.list;
+      this.$state.go('editor', {id: '0', name: '', path: ''});
+    }, (error) => {
+      console.log(error.message);
+      this.catalog = undefined;
+      this.path = '';
+    });
+  }
   updateCatalog(command, list, path, newFileID) {
     if (command === 'save' || command === 'move') {
       this.catalog = list;
