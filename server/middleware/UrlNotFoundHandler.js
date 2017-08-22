9df7923f639f75115c815a4157ec420fd9301149
Klainti <kbontourri@gmail.com>
2017-08-22 14:45:54 +0300

    Simplify

--- server/middleware/UrlNotFoundHandler.js
+++ server/middleware/UrlNotFoundHandler.js
@@ -2,13 +2,14 @@
 
 const path = require('path');
 
+/* UrlNotFoundHandler. Send index if url not found! */
 module.exports = function () {
-  return function customRaiseUrlNotFoundError(req, res) {
+  return ((req, res) => {
     res.sendFile(path.join(__dirname, '../../client/index.html'), (err) => {
       if (err) {
-        console.error(err);
         res.status(err.status).end();
       }
     });
-  };
+  });
 };
+
