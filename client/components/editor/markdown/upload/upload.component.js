60a8ba08b29a7d97a1b9bafbf88c3e0f2beef490
vchristodoulou <vchristodoulou1@gmail.com>
2017-08-22 17:26:27 +0300

    upload picture from web or local, updated views, local not working

--- client/components/editor/markdown/upload/upload.component.js
+++ client/components/editor/markdown/upload/upload.component.js
@@ -1,14 +1,33 @@
 'use strict';
 
 class UploadPopUpController {
-  constructor($mdDialog) {
+  constructor($mdDialog, Upload) {
     this.$mdDialog = $mdDialog;
+    this.Upload = Upload;
+    this.insertFlag = true;
   }
-  ok() {
-    this.$mdDialog.hide({ picURL: this.picURL, picName: this.picName });
+  ok(insertFlag, urlErr, nameErr) {
+    if (!urlErr.required && !nameErr.required) {
+      this.$mdDialog.hide({picURL: this.picURL, picName: this.picName, insertFlag});
+    }
   }
   cancel() {
     this.$mdDialog.cancel();
+  }
+  submit() {
+    console.log('SUBMIT LOCAL');
+    console.log(this.picFile);
+    this.Upload.upload({
+      url: '/Pictures',
+      data: { file: this.picFile }
+    }).then(function (resp) {
+      console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+    }, function (resp) {
+      console.log('Error status: ' + resp.status);
+    }, function (evt) {
+      var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+      console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+    });
   }
 }
 
@@ -24,8 +43,10 @@ class UploadController {
       controllerAs: 'ctrl',
       clickOutsideToClose: true
     }).then((res) => {
-      this.Picture.create({ name: res.picName, url: res.picURL }, (res) => {
-        console.log(res.message);
+      this.Picture.create({ name: res.picName, url: res.picURL }, (data) => {
+        if (res.insertFlag) {
+          this.onInsertPicture({id: data.id});
+        }
       }, (error) => {
         console.log(error.message);
       });
@@ -40,7 +61,7 @@ angular.module('UserGuideApp')
       controller: UploadController,
       templateUrl: 'components/editor/markdown/upload/upload.view.html',
       bindings: {
-        onMarkdownUpload: '&'
+        onInsertPicture: '&'
       }
     });
 
